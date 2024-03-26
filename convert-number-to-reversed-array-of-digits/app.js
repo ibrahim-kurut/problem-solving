45db59ec8304d4e3feb0cea26aeb3842e1873cc7
ibrahim kurut <102996120+ibrahim-kurut@users.noreply.github.com>
2024-03-26 10:25:02 +0300

    convert number to reversed array of digits

---
+++ convert-number-to-reversed-array-of-digits/app.js
@@ -0,0 +1,12 @@
+//! Solution steps
+// convert number to string 
+// convert string to array 
+// reverse this array
+
+const convertNumberToReversedArray = (nums) => {
+    return nums = nums.toString().split("").reverse()
+}
+console.log(convertNumberToReversedArray(2567894));
+
+
+
